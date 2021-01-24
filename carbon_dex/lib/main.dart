64769cca30aaed2b29b563ce552af4ecd4d2f24b
Ashwani Rathee <ab669522@gmail.com>
2021-01-24 11:30:44 +0530

    Routing to forms added

--- carbon_dex/lib/main.dart
+++ carbon_dex/lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:carbon_dex/screens/intro.dart';
 import 'package:flutter/material.dart';
 import './screens/splashscreen.dart';
+import './screens/forms.dart';
 
 var routes = <String, WidgetBuilder>{
   "/": (BuildContext context) => SplashScreen(),
   "/home": (BuildContext context) => IntroScreen(),
+  "/form": (BuildContext context) => FormScreen(),
 };
 
 void main() => runApp(
