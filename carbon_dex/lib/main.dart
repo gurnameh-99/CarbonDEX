e7a3a78ea0dd47a4ac7d4178155d3acb2b7d4005
Ashwani Rathee <ab669522@gmail.com>
2021-01-24 08:00:33 +0530

    SplashScreen Implemented

--- carbon_dex/lib/main.dart
+++ carbon_dex/lib/main.dart
@@ -1,18 +1,11 @@
 import 'package:flutter/material.dart';
-import './screens/intro.dart';
+import './screens/splashscreen.dart';
 
-void main() => runApp(Home());
-
-class Home extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'CarbonFootprint App',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+void main() => runApp(
+      MaterialApp(
+        theme: ThemeData(
+            primaryColor: Colors.red, accentColor: Colors.yellowAccent),
+        debugShowCheckedModeBanner: false,
+        home: SplashScreen(),
       ),
-      home: IntroScreen(),
     );
-  }
-}
-
